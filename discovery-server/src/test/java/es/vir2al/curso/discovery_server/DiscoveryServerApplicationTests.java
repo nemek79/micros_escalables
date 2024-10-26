33d3f480e81f2baa4e63dc8c6b8a3c7cbfb7d38a
David Alonso <davidalonso79@gmail.com>
2024-10-26 19:50:22 +0200

    dev initial version of eureka version

---
+++ discovery-server/src/test/java/es/vir2al/curso/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vir2al.curso.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
