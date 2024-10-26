ffbdd7e7ec048777d66bf36aee9501a7d7cf914b
David Alonso <davidalonso79@gmail.com>
2024-10-26 18:56:51 +0200

    fix (inventory) delete all products before loading

--- inventory-service/src/main/java/es/vir2al/curso/inventory_service/utils/DataLoader.java
+++ inventory-service/src/main/java/es/vir2al/curso/inventory_service/utils/DataLoader.java
@@ -21,6 +21,8 @@ public class DataLoader implements CommandLineRunner {
 
         log.info("loading data...");
 
+        inventoryRepository.deleteAll();
+
         inventoryRepository.saveAll(
           List.of(
                   Inventory.builder().sku("000001").quantity(10L).build(),
